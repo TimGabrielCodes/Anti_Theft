fff3d1dbb4a50d8938fb871dd488abf4d23c107d
Bobby Ekaro <iambobbymax@gmail.com>
2020-07-08 04:35:30 +0100

    First Commit - Implemented the Fetch of installed applications.

--- app/src/main/java/com/example/antitheft/MainActivity.java
+++ app/src/main/java/com/example/antitheft/MainActivity.java
@@ -7,27 +7,102 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
+import java.util.List;
+import io.paperdb.Paper;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button  signInBtn;
+    private Button signInBtn;
+    String saved_pattern_key = "pattern_code";
+    PatternLockView mPatternLockView;
+    String final_pattern = "final_pattern";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    signInBtn = findViewById(R.id.btn_signin);
 
-    signInBtn.setOnClickListener(new View.OnClickListener(){
+        Paper.init(this);
+        final String saved_pattern = Paper.book().read(saved_pattern_key);
 
-        public void onClick(View v){
-            moveToHomeScreen();
+        if (saved_pattern != null && !saved_pattern.equals("null")) {
+
+            setContentView(R.layout.pattern_screen);
+            mPatternLockView = (PatternLockView) findViewById(R.id.pattern_lock_view);
+            mPatternLockView.addPatternLockListener(new PatternLockViewListener() {
+                @Override
+                public void onStarted() {
+
+                }
+
+                @Override
+                public void onProgress(List<PatternLockView.Dot> progressPattern) {
+
+                }
+
+                @Override
+                public void onComplete(List<PatternLockView.Dot> pattern) {
+                    final_pattern = PatternLockUtils.patternToString(mPatternLockView, pattern);
+                    if (final_pattern.equals(saved_pattern)){
+                        Toast.makeText(MainActivity.this, "Pattern Correct!", Toast.LENGTH_SHORT).show();
+                        moveToHomeScreen();
+                    }
+
+                    else {
+                        Toast.makeText(MainActivity.this, "Pattern Incorrect", Toast.LENGTH_SHORT);
+                    }
+                }
+
+                @Override
+                public void onCleared() {
+
+                }
+            });
+        } else {
+            setContentView(R.layout.activity_lock_screen);
+            mPatternLockView = (PatternLockView) findViewById(R.id.pattern_lock_view);
+            mPatternLockView.addPatternLockListener(new PatternLockViewListener() {
+                @Override
+                public void onStarted() {
+                    Toast.makeText(MainActivity.this, "Set  Lock Screen Pattern" , Toast.LENGTH_SHORT);
+                }
+
+                @Override
+                public void onProgress(List<PatternLockView.Dot> progressPattern) {
+
+                }
+
+                @Override
+                public void onComplete(List<PatternLockView.Dot> pattern) {
+                    final_pattern = PatternLockUtils.patternToString(mPatternLockView, pattern);
+                }
+
+                @Override
+                public void onCleared() {
+
+                }
+            });
+
+            Button btnSetup = (Button) findViewById(R.id.btnSetPattern);
+
+            btnSetup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Paper.book().write(saved_pattern_key, final_pattern);
+                    Toast.makeText(MainActivity.this, "Pattern Saved", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            });
+
         }
 
-    });
 
     }
-    private void  moveToHomeScreen(){
+
+    private void moveToHomeScreen() {
 
         Intent intent = new Intent(MainActivity.this, HomeScreen.class);
         startActivity(intent);
